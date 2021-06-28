4bde7b9ce8b312d2218afabf70ed518005a7737d
Nmobki <nmo@bki.dk>
2021-06-28 11:12:20 +0200

    Update Sporbarhed_færdigkaffe.py

--- Sporbarhed_færdigkaffe.py
+++ Sporbarhed_færdigkaffe.py
@@ -301,6 +301,59 @@ query_nav_generelt = f""" WITH [RECEPT] AS (
 df_nav_generelt = pd.read_sql(query_nav_generelt, con_nav)
 
 
+# Recursive query to find all relevant produced orders related to the requested order
+# First is identified all lotnumbers related to the orders identified through NAV reservations (only production orders)
+# Next is a recursive part which identifies any document numbers which have consumed these lotnumbers (ILE_C)
+# Which is then queried again to find all lotnumbers produced on the orders from which these lotnumbers originally came.
+query_nav_færdigvaretilgang = f""" WITH [LOT_ORG] AS ( 
+SELECT
+	[Lot No_]
+FROM [dbo].[BKI foods a_s$Item Ledger Entry]
+WHERE [Order No_] IN( '036720')
+	AND [Entry Type] = 6
+
+UNION ALL
+
+SELECT
+	ILE_O.[Lot No_]
+FROM [LOT_ORG]
+INNER JOIN [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE_C
+	ON [LOT_ORG].[Lot No_] = ILE_C.[Lot No_]
+	AND [ILE_C].[Entry Type] IN (5,8)
+INNER JOIN [dbo].[BKI foods a_s$Item Ledger Entry]  AS ILE_O
+	ON ILE_C.[Document No_] = ILE_O.[Document No_]
+	AND ILE_O.[Entry Type] IN (6,9)
+)
+
+
+SELECT
+	ILE.[Item No_]
+	,I.[Description]
+	,SUM(CASE
+		WHEN ILE.[Entry Type] IN (0,6,9)
+			THEN ILE.[Quantity] * I.[Net Weight]
+		ELSE 0
+	END) AS [Produceret]
+	,SUM(CASE
+		WHEN ILE.[Entry Type] = 1
+			THEN ILE.[Quantity] * I.[Net Weight] * -1
+		ELSE 0
+	END) AS [Salg]
+	,SUM(CASE
+		WHEN ILE.[Entry Type] IN (2,3,5,8)
+			THEN ILE.[Quantity] * I.[Net Weight] * -1
+		ELSE 0
+	END) AS [Regulering & ompak]
+	,SUM(ILE.[Remaining Quantity]) AS [Restlager]
+FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+	ON ILE.[Item No_] = I.[No_]
+INNER JOIN [LOT_ORG]
+	ON ILE.[Lot No_] = LOT_ORG.[Lot No_]
+GROUP BY
+	ILE.[Item No_]
+	,I.[Description] """
+
 query_nav_lotno = f""" SELECT ILE.[Lot No_] AS [Lotnummer]
             	  ,LI.[Certificate Number] AS [Pallenummer]
               	  ,[Quantity] * I.[Net Weight] AS [Kilo]

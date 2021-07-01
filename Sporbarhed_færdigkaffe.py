bceacec95155f1317f6f70a4228c3154ed948d32
Nmobki <nmo@bki.dk>
2021-07-01 15:42:58 +0200

    Update Sporbarhed_færdigkaffe.py
    page orientation

--- Sporbarhed_færdigkaffe.py
+++ Sporbarhed_færdigkaffe.py
@@ -69,20 +69,19 @@ def convert_placeholders_word(string):
     else:
         return string
 
-# # Change Word page format between landscape and horizontal
-# def page_orientation_word(orientation):
-#     x = 21
-#     y = 29.7
-#     new_section
-#     if orientation == 'Portrait':
-#         pass
-#     elif orientation == 'Landscape':
-#         pass
-#     else:
-#         return None
+# Change Word page format between landscape and horizontal
+def page_orientation_word(orientation):
+    x = 7772400
+    y = 10058400
+    if orientation == 'Portrait':
+        return [x,y]
+    elif orientation == 'Landscape':
+        return [y,x]
+    else:
+        return None
 
 # Add dataframe to word document
-def add_section_to_word(dataframe,section, pagebreak):
+def add_section_to_word(dataframe, section, pagebreak, orientation):
     # Add section header
     doc.add_heading(section, 1)
     # Add a table with an extra row for headers
@@ -99,6 +98,9 @@ def add_section_to_word(dataframe,section, pagebreak):
     # Add page break
     if pagebreak == True:
         doc.add_page_break()  
+        # doc.sections[-1].orientation = docx.enum.section.WD_ORIENT.LANDSCAPE # Tænker måske denne her er ligegyldig
+        doc.sections[-1].page_width = page_orientation_word(orientation)[0] #10058400
+        doc.sections[-1].page_height = page_orientation_word(orientation)[1] #7772400
 
 # =============================================================================
 # Variables for query connections
@@ -556,8 +558,7 @@ if get_section_status_code(df_results_generelt, get_section_visibility(df_sectio
         df_results_generelt = df_results_generelt[column_order].transpose()        
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_results_generelt, section_name, True)
-        # *** TO DO: Insert into Word
-        add_section_to_word(df_results_generelt, section_name, True)
+        add_section_to_word(df_results_generelt, section_name, True, 'Portrait')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -579,7 +580,7 @@ if get_section_status_code(df_nav_færdigvaretilgang, get_section_visibility(df_
         df_nav_færdigvaretilgang = df_nav_færdigvaretilgang[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_nav_færdigvaretilgang, section_name, False)
-        add_section_to_word(df_nav_færdigvaretilgang, section_name, True)
+        add_section_to_word(df_nav_færdigvaretilgang, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -604,7 +605,7 @@ if get_section_status_code(df_probat_ulg, get_section_visibility(df_sections, se
         df_probat_ulg = df_probat_ulg[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_probat_ulg, section_name, False)
-        add_section_to_word(df_probat_ulg, section_name, True)
+        add_section_to_word(df_probat_ulg, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -629,7 +630,7 @@ if get_section_status_code(df_probat_ulr, get_section_visibility(df_sections, se
         df_probat_ulr = df_probat_ulr[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_probat_ulr, section_name, False)
-        add_section_to_word(df_probat_ulr, section_name, True)
+        add_section_to_word(df_probat_ulr, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -646,14 +647,13 @@ section_name = get_section_name(section_id)
 timestamp = datetime.now()
 column_order = ['Sortnummer','Sortnavn','Silo','Kontraktnummer','Modtagelse',
                 'Ordrenummer','Kilo']
-
 if get_section_status_code(df_probat_lr, get_section_visibility(df_sections, section_id)) == 99:
     try:
         df_probat_lr['Sortnavn'] = df_probat_lr['Sortnummer'].apply(get_nav_item_info, field='Beskrivelse')
         df_probat_lr = df_probat_lr[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_probat_lr, section_name, False)
-        add_section_to_word(df_probat_lr, section_name, True)
+        add_section_to_word(df_probat_lr, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -676,7 +676,7 @@ if get_section_status_code(df_nav_debitorer, get_section_visibility(df_sections,
         df_nav_debitorer = df_nav_debitorer[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_nav_debitorer, section_name, False)
-        add_section_to_word(df_nav_debitorer, section_name, True)
+        add_section_to_word(df_nav_debitorer, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -718,7 +718,7 @@ if get_section_status_code(df_massebalance, get_section_visibility(df_sections, 
     try:
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_massebalance, section_name, True)
-        add_section_to_word(df_massebalance, section_name, True)
+        add_section_to_word(df_massebalance, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -741,7 +741,7 @@ if get_section_status_code(df_com_statistics, get_section_visibility(df_sections
         df_com_statistics = df_com_statistics[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_com_statistics, section_name, False)
-        add_section_to_word(df_com_statistics, section_name, True)
+        add_section_to_word(df_com_statistics, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -762,7 +762,7 @@ if get_section_status_code(df_karakterer, get_section_visibility(df_sections, se
         df_karakterer['Dato'] = df_karakterer['Dato'].dt.strftime('%d-%m-%Y')
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_karakterer, section_name, False)
-        add_section_to_word(df_karakterer, section_name, True)
+        add_section_to_word(df_karakterer, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -784,7 +784,7 @@ if get_section_status_code(df_nav_consumption, get_section_visibility(df_section
         df_nav_consumption = df_nav_consumption[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_nav_consumption, section_name, False)
-        add_section_to_word(df_nav_consumption, section_name, True)
+        add_section_to_word(df_nav_consumption, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -809,7 +809,7 @@ if get_section_status_code(df_nav_components, get_section_visibility(df_sections
         df_nav_components = df_nav_components[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_nav_components, section_name, False)
-        add_section_to_word(df_nav_components, section_name, True)
+        add_section_to_word(df_nav_components, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -839,7 +839,7 @@ if get_section_status_code(df_karakterer, get_section_visibility(df_sections, se
         df_nav_lotno = df_nav_lotno[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_nav_lotno, section_name, False)
-        add_section_to_word(df_nav_lotno, section_name, True)
+        add_section_to_word(df_nav_lotno, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -864,7 +864,7 @@ if get_section_status_code(df_temp, get_section_visibility(df_sections, section_
         df_temp = df_temp[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_temp, section_name, False)
-        add_section_to_word(df_temp, section_name, True)
+        add_section_to_word(df_temp, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -890,7 +890,7 @@ if get_section_status_code(df_temp, get_section_visibility(df_sections, section_
         df_temp = df_temp[column_order]
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_temp, section_name, False)
-        add_section_to_word(df_temp, section_name, True)
+        add_section_to_word(df_temp, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -911,7 +911,7 @@ if get_section_status_code(df_section_log, get_section_visibility(df_sections, s
     try:
         # Write results to Word and Excel
         insert_dataframe_into_excel (df_section_log, section_name, False)
-        add_section_to_word(df_section_log, section_name, True)
+        add_section_to_word(df_section_log, section_name, True, 'Landscape')
         # Write status into log
         section_log_insert(timestamp, section_id, 0)
     except: # Insert error into log
@@ -922,10 +922,10 @@ else: # Write into log if no data is found or section is out of scope
 
 #Save files
 excel_writer.save()
-log_insert(script_name, 'Excel file created')
+log_insert(script_name, f'Excel file {file_name} created')
 
 doc.save(path_file_doc)
-log_insert(script_name, 'Word document created')
+log_insert(script_name, f'Word document {file_name} created')
 # *** TODO SAVE PDF FILE
 
 # =============================================================================

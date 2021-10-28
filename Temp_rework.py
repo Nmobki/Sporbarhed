cf84a149cf8a65db5c775acad16f8817b65aa49e
Nmobki <nmo@bki.dk>
2021-10-28 12:37:06 +0200

    Update Temp_rework.py

--- Temp_rework.py
+++ Temp_rework.py
@@ -34,15 +34,12 @@ engine_probat = create_engine(f'mssql+pyodbc:///?odbc_connect={params_probat}')
 # =============================================================================
 
 dict_temp_silos = {
-            'Dato': ['2021-09-21','2021-09-22','2021-09-17','2021-09-16']
-            ,'Startdato': ['2021-09-17','2021-09-20','2021-09-17','2021-09-10']
-            ,'Slutdato': ['2021-09-21','2021-09-25','2021-09-21','2021-09-16']
+            'Startdato': ['2021-09-17','2021-09-20','2021-09-17','2021-09-10']
+            ,'Slutdato': ['2021-09-21','2021-09-22','2021-09-21','2021-09-16']
             ,'Silo': ['401','401','511','512'] 
             ,'Ordrenummer': ['O1','O2','O3','04']
             }
 df_temp_silos = pd.DataFrame.from_dict(dict_temp_silos)
-#Slutdato 2 skal implementeres i sidste udgave. Det giver ikke mening at spore på noget, der er tilsat efter produktion
-df_temp_silos['Slutdato2'] = df_temp_silos[['Dato','Slutdato']].min(axis=1)
 
 print(df_temp_silos)
 
@@ -86,14 +83,15 @@ def get_nav_order_info(order_no):
 # New functions - to be implemented in main script
 # =============================================================================
 def get_rework_silos():
-    pass
+    pass # Query that returns dataframe with requested orders, silos and dates for relevant orders.
+    # Return None if no orders relevant.
 
 
 def get_rework_prøvesmagning(start_date, end_date, silo, order_no):
-    if None in (start_date, end_date, silo):
+    if None in (start_date, end_date, silo, order_no):
         return None
     else:
-        query = f""" SELECT	RP.[Produktionsordrenummer]
+        query = f""" SELECT	RP.[Produktionsordrenummer] AS [Indhold]
                 FROM [cof].[Rework_tilgang] AS RT
                 INNER JOIN [cof].[Rework_prøvesmagning] AS RP
                     ON RT.[Referencenummer] = RP.[Referencenummer]
@@ -107,9 +105,55 @@ def get_rework_prøvesmagning(start_date, end_date, silo, order_no):
         else:
             df_temp['Silo'] = silo
             df_temp['Ordrenummer'] = order_no
+            df_temp['Kilde'] = 'Prøvesmagning'
             return df_temp
 
+def get_rework_pakkeri(start_date, end_date, silo, order_no):
+    if None in (start_date, end_date, silo, order_no):
+        return None
+    else:
+        query_ds = f""" SELECT DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) AS [Dato]
+                   ,[Silo],[Reworktype]
+                   FROM [BKI_Datastore].[cof].[Rework_tilgang]
+                   WHERE Kilde = 1 AND [Silo] = '{silo}'
+                   AND DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) BETWEEN '{start_date}' AND '{end_date}'
+                   GROUP BY
+                   DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0)
+                   ,[Silo],[Reworktype] """
+        df_ds = pd.read_sql(query_ds, con_04)
+        df_total = pd.DataFrame()
+        if len(df_ds) == 0:
+            return None
+        else:
+            for i in df_ds.index:
+                dato = df_ds['Dato'][i].strftime('%Y-%m-%d')
+                rework_type = df_ds['Reworktype'][i]
+                query_nav = f""" WITH NAV_CTE AS ( SELECT ILE.[Posting Date] AS [Dato]
+                        	,ILE.[Document No_] AS [Indhold]
+                        	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                            	ELSE 1 END AS [Kaffetype]
+                            FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+                            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+                            	ON ILE.[Item No_] = I.[No_]
+                            WHERE ILE.[Entry Type] = 6
+                            	AND I.[Item Category Code] = 'FÆR KAFFE'
+                            GROUP BY ILE.[Posting Date] ,ILE.[Item No_],ILE.[Document No_]
+                            	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                            	ELSE 1 END )
+                            SELECT * FROM NAV_CTE WHERE [Dato] = '{dato}' AND [Kaffetype] = {rework_type} """
+                df_nav = pd.read_sql(query_nav, con_nav)
+                df_total = pd.concat([df_total, df_nav])
+        if len(df_total) == 0:
+            return None
+        else:
+            df_total['Silo'] = silo
+            df_total['Ordrenummer'] = order_no
+            df_total['Kilde'] = 'Pakkeri'
+            return df_total
+                
 
+                
+        
 
 
 
@@ -123,13 +167,16 @@ def get_rework_prøvesmagning(start_date, end_date, silo, order_no):
 if len(df_temp_silos) > 0:
     for i in df_temp_silos.index:
         startdato = df_temp_silos['Startdato'][i]
-        slutdato = df_temp_silos['Slutdato2'][i]
+        slutdato = df_temp_silos['Slutdato'][i]
         silo = df_temp_silos['Silo'][i]
         ordrenummer = df_temp_silos['Ordrenummer'][i]
         # print(df_temp_silos['Startdato'][i], i)
         
-        print(get_rework_prøvesmagning(startdato, slutdato, silo, ordrenummer))
-        get_rework_prøvesmagning(startdato, slutdato, silo, ordrenummer)
+        # print(get_rework_prøvesmagning(startdato, slutdato, silo, ordrenummer))
+
+        print(get_rework_pakkeri(startdato, slutdato, silo, ordrenummer))
+
+    
         
     
 #    Kør hver funktion og gem dem i en samlet dataframe

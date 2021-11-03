393add90eb3ef50f304de6a47519fa7838d3cc37
Nmobki <nmo@bki.dk>
2021-11-03 10:27:24 +0100

    Update Sporbarhed_shared_functions.py

--- Sporbarhed_shared_functions.py
+++ Sporbarhed_shared_functions.py
@@ -208,6 +208,179 @@ def get_email_subject(request_reference, request_type):
     return str(dict_email_subject[request_type])
 
 
-
-
-
+class rework():
+    def get_silo_last_empty(silo, date):
+        query = f""" SELECT	MAX(DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0)) AS [Dato]
+                     FROM [dbo].[PRO_EXP_SILO_DIF]
+                     WHERE [SILO] = '{silo}'
+                     DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0) < '{date}' """
+        df = pd.read_sql(query, con_probat)
+        if len(df) == 0:
+            return None
+        else:
+            df['Dato'].strftime('%Y-%m-%d')
+            return str(df['Dato'].iloc[0])
+    
+    def get_rework_silos(orders_string):
+        query = f""" SELECT DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0) AS [Slutdato]
+                    ,[SOURCE_NAME] AS [Silo] ,[ORDER_NAME] AS [Ordrenummer]
+                    FROM [dbo].[PRO_EXP_ORDER_UNLOAD_G]
+                    WHERE [SOURCE_NAME] IN ('511','512') AND [ORDER_NAME] IN ({orders_string})
+                    GROUP BY DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0) ,[SOURCE_NAME] ,[ORDER_NAME]
+                    UNION ALL
+                    SELECT	DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0)
+                    ,[SOURCE] ,[ORDER_NAME]
+                    FROM [dbo].[PRO_EXP_ORDER_LOAD_G]
+                    WHERE [SOURCE] in ('401','403') AND [ORDER_NAME] IN ({orders_string})
+                    GROUP BY DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0)
+                    ,[SOURCE] ,[ORDER_NAME] """
+        df = pd.read_sql(query, con_probat)
+        if len(df) == 0:
+            return None
+        else:      
+            df['Startdato'] = df['Silo'].apply((lambda x: rework.get_silo_last_empty(x, df['Slutdato'].strftime('%Y-%m-%d'))))
+            return df
+    
+    def get_rework_prøvesmagning(start_date, end_date, silo, order_no):
+        if None in (start_date, end_date, silo, order_no):
+            return None
+        else:
+            query = f""" SELECT	RP.[Produktionsordrenummer] AS [Indhold]
+                    FROM [cof].[Rework_tilgang] AS RT
+                    INNER JOIN [cof].[Rework_prøvesmagning] AS RP
+                        ON RT.[Referencenummer] = RP.[Referencenummer]
+                    WHERE RT.[Kilde] = 0
+                        AND RT.[Silo] = '{silo}'
+                        AND DATEADD(D, DATEDIFF(D, 0, RT.[Registreringstidspunkt] ), 0) BETWEEN '{start_date}' AND '{end_date}'
+                    GROUP BY RP.[Produktionsordrenummer] """
+            df_temp = pd.read_sql(query, con_04)
+            if len(df_temp) == 0:
+                return None
+            else:
+                df_temp['Silo'] = silo
+                df_temp['Ordrenummer'] = order_no
+                df_temp['Kilde'] = 'Prøvesmagning'
+                return df_temp
+    
+    def get_rework_pakkeri(start_date, end_date, silo, order_no):
+        if None in (start_date, end_date, silo, order_no):
+            return None
+        else:
+            query_ds = f""" SELECT DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) AS [Dato]
+                       ,[Silo],[Reworktype]
+                       FROM [BKI_Datastore].[cof].[Rework_tilgang]
+                       WHERE Kilde = 1 AND [Silo] = '{silo}'
+                       AND DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) BETWEEN '{start_date}' AND '{end_date}'
+                       GROUP BY
+                       DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0)
+                       ,[Silo],[Reworktype] """
+            df_ds = pd.read_sql(query_ds, con_04)
+            df_total = pd.DataFrame()
+            if len(df_ds) == 0:
+                return None
+            else:
+                for i in df_ds.index:
+                    dato = df_ds['Dato'][i].strftime('%Y-%m-%d')
+                    rework_type = df_ds['Reworktype'][i]
+                    query_nav = f""" WITH NAV_CTE AS ( SELECT ILE.[Posting Date] AS [Dato]
+                            	,ILE.[Document No_] AS [Indhold]
+                            	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                                	ELSE 1 END AS [Kaffetype]
+                                FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+                                INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+                                	ON ILE.[Item No_] = I.[No_]
+                                WHERE ILE.[Entry Type] = 6
+                                	AND I.[Item Category Code] = 'FÆR KAFFE'
+                                GROUP BY ILE.[Posting Date] ,ILE.[Item No_],ILE.[Document No_]
+                                	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                                	ELSE 1 END )
+                                SELECT * FROM NAV_CTE WHERE [Dato] = '{dato}' AND [Kaffetype] = {rework_type} """
+                    df_nav = pd.read_sql(query_nav, con_nav)
+                    df_total = pd.concat([df_total, df_nav])
+            if len(df_total) == 0:
+                return None
+            else:
+                df_total['Silo'] = silo
+                df_total['Ordrenummer'] = order_no
+                df_total['Kilde'] = 'Pakkeri'
+                return df_total
+    
+    def get_rework_komprimatorrum(start_date, end_date, silo, order_no):
+        if None in (start_date, end_date, silo, order_no):
+            return None
+        else:
+            query_ds = f""" SELECT [Referencenummer] AS [Indhold]
+                       FROM [BKI_Datastore].[cof].[Rework_tilgang]
+                       WHERE Kilde = 2 AND [Silo] = '{silo}'
+                       AND DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) BETWEEN '{start_date}' AND '{end_date}'
+                       GROUP BY [Referencenummer] """
+            df_ds = pd.read_sql(query_ds, con_04)
+            if len(df_ds) == 0:
+                return None
+            else:
+                df_ds['Silo'] = silo
+                df_ds['Ordrenummer'] = order_no
+                df_ds['Kilde'] = 'Komprimatorrum'
+                return df_ds
+    
+    def get_rework_henstandsprøver(start_date, end_date, silo, order_no):
+        if None in (start_date, end_date, silo, order_no):
+            return None
+        else:
+            query_ds = f""" SELECT [Startdato] AS [Dato]
+                       ,[Silo],[Reworktype]
+                       FROM [BKI_Datastore].[cof].[Rework_tilgang]
+                       WHERE Kilde = 3 AND [Silo] = '{silo}'
+                       AND DATEADD(D, DATEDIFF(D, 0, [Registreringstidspunkt] ), 0) BETWEEN '{start_date}' AND '{end_date}'
+                       GROUP BY [Startdato],[Silo],[Reworktype] """
+            df_ds = pd.read_sql(query_ds, con_04)
+            df_total = pd.DataFrame()
+            if len(df_ds) == 0:
+                return None
+            else:
+                for i in df_ds.index:
+                    dato = df_ds['Dato'][i].strftime('%Y-%m-%d')
+                    rework_type = df_ds['Reworktype'][i]
+                    query_nav = f""" WITH NAV_CTE AS ( SELECT ILE.[Posting Date] AS [Dato]
+                            	,ILE.[Document No_] AS [Indhold]
+                            	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                                	ELSE 1 END AS [Kaffetype]
+                                FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+                                INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+                                	ON ILE.[Item No_] = I.[No_]
+                                WHERE ILE.[Entry Type] = 6
+                                	AND I.[Item Category Code] = 'FÆR KAFFE'
+                                GROUP BY ILE.[Posting Date] ,ILE.[Item No_],ILE.[Document No_]
+                                	,CASE WHEN I.[Produktionskode] LIKE '%HB' THEN 2
+                                	ELSE 1 END )
+                                SELECT * FROM NAV_CTE WHERE [Dato] = '{dato}' AND [Kaffetype] = {rework_type} """
+                    df_nav = pd.read_sql(query_nav, con_nav)
+                    df_total = pd.concat([df_total, df_nav])
+            if len(df_total) == 0:
+                return None
+            else:
+                df_total['Silo'] = silo
+                df_total['Ordrenummer'] = order_no
+                df_total['Kilde'] = 'Henstandsprøver'
+                return df_total
+    
+    def get_rework_total(df_silos):
+        if len(df_silos) == 0:
+            return pd.DataFrame()
+        else:
+            df_rework = pd.DataFrame()
+            for i in df_silos.index:
+                startdato = df_silos['Startdato'][i]
+                slutdato = df_silos['Slutdato'][i]
+                silo = df_silos['Silo'][i]
+                ordrenummer = df_silos['Ordrenummer'][i]
+                # Functions to get each different type of rework
+                df_prøvesmagning = rework.get_rework_prøvesmagning(startdato, slutdato, silo, ordrenummer)
+                df_pakkeri = rework.get_rework_pakkeri(startdato, slutdato, silo, ordrenummer)
+                df_komprimatorrum = rework.get_rework_komprimatorrum(startdato, slutdato, silo, ordrenummer)
+                df_henstandsprøver = rework.get_rework_henstandsprøver(startdato, slutdato, silo, ordrenummer)
+                # Concat each function to one dataframe
+                df_rework = pd.concat([df_rework, df_prøvesmagning, df_pakkeri, df_komprimatorrum, df_henstandsprøver])
+        return df_rework[['Ordrenummer','Silo','Indhold','Kilde']]
+    
+    

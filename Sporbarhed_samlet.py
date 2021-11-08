fb1e1419ed5f5a30d98738c17356c17fcfe1bd19
Nmobki <nmo@bki.dk>
2021-11-08 09:45:11 +0100

    Update Sporbarhed_samlet.py
    Added use of new functions to fetch server connections and removed old hard coded connections.
    renamed 04 to ds

--- Sporbarhed_samlet.py
+++ Sporbarhed_samlet.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import pandas as pd
-import pyodbc
 import Sporbarhed_råkaffe
 import Sporbarhed_færdigkaffe
 import Sporbarhed_opspræt
+import Sporbarhed_shared_functions as ssf
 
 
 # =============================================================================
 # Variables for query connections
 # =============================================================================
-server_04 = 'sqlsrv04'
-db_04 = 'BKI_Datastore'
-con_04 = pyodbc.connect(f'DRIVER=SQL Server;SERVER={server_04};DATABASE={db_04};autocommit=True')
+con_ds = ssf.get_connection('bki_datastore')
 
 # =============================================================================
 # Read data from request
@@ -23,7 +20,7 @@ query_ds_request =  """ SELECT TOP 1 [Id] ,[Forespørgselstype],[Rapport_modtage
                     ,[Referencenummer] ,[Note_forespørgsel] ,[Modtagelse]  ,[Ordrerelationstype]
                     FROM [trc].[Sporbarhed_forespørgsel]
                     WHERE [Forespørgsel_igangsat] IS NULL """
-df_request = pd.read_sql(query_ds_request, con_04)
+df_request = pd.read_sql(query_ds_request, con_ds)
 
 # Exit script if no request data is found
 if len(df_request) == 0:
@@ -45,7 +42,6 @@ elif req_type == 1:
     Sporbarhed_råkaffe.initiate_report(req_id)
 elif req_type == 2:
     Sporbarhed_opspræt.initiate_report(req_id)
-
 
 # Exit script
 raise SystemExit(0)

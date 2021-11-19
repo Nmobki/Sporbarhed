f5bc690e52db27d5d0ff171d14743726f5aafe4b
Nmobki <nmo@bki.dk>
2021-11-19 14:24:19 +0100

    Update Sporbarhed_emballage.py
    Section 1

--- Sporbarhed_emballage.py
+++ Sporbarhed_emballage.py
@@ -19,7 +19,7 @@ def initiate_report(initiate_id):
     # Read data from request
     # =============================================================================
     query_ds_request =  f""" SELECT TOP 1 [Id] ,[Forespørgselstype],[Rapport_modtager]
-                        ,[Referencenummer] ,[Note_forespørgsel] ,[Modtagelse]  ,[Ordrerelationstype]
+                        ,[Referencenummer] ,[Note_forespørgsel] ,[Modtagelse]  ,[Referencetype]
                         FROM [trc].[Sporbarhed_forespørgsel]
                         WHERE [Id] = {initiate_id} """
     df_request = pd.read_sql(query_ds_request, con_ds)
@@ -32,14 +32,13 @@ def initiate_report(initiate_id):
     # =============================================================================
     req_type = df_request.loc[0, 'Forespørgselstype']
     req_reference_no = df_request.loc[0, 'Referencenummer'].rstrip(' ')
+    req_reference_type = df_request.loc[0, 'Referencetype']
     req_recipients = df_request.loc[0, 'Rapport_modtager']
     req_note = df_request.loc[0, 'Note_forespørgsel']
     req_id = df_request.loc[0, 'Id']
-    req_ordrelationstype = df_request.loc[0, 'Ordrerelationstype']
+    req_roll = df_request.loc[0, 'Modtagelse']
 
-    script_name = 'Sporbarhed_færdigkaffe.py'
-    orders_top_level = [req_reference_no]
-    orders_related = []
+    script_name = 'Sporbarhed_emballage.py'
     # Read setup for section for reporttypes. NAV querys with NOLOCK to prevent deadlocks
     df_sections = ssf.get_ds_reporttype(req_type)
 
@@ -64,3 +63,107 @@ def initiate_report(initiate_id):
 
     png_relations_name = f'{file_name}.png'
     path_png_relations = filepath + r'\\' + png_relations_name
+
+    # =============================================================================
+    # Queries for each different type of referencetype and requesttype     
+    # Queries are defined individually, but only used depending on reference and request type.
+    # =============================================================================
+    # Query Navision for information with refencetype = 4 (lot no) and no roll number. 
+    query_nav_lot = f"""SELECT POAC.[Prod_ Order No_] AS [Produktionsordrenummer]
+                    	,POC.[Item No_] AS [Varenummer], [Roll No_] AS [Rullenummer]
+                        FROM [dbo].[BKI foods a_s$Prod_ Order Add_ Comp_] AS POAC
+                        INNER JOIN [dbo].[BKI foods a_s$Prod_ Order Component] AS POC
+                        	ON POAC.[Prod_ Order No_] = POC.[Prod_ Order No_]
+                        	AND POAC.[Prod_ Order Line No_] = POC.[Prod_ Order Line No_]
+                        	AND POAC.[Prod_ Order Component Line No_] = POC.[Line No_]
+                        WHERE POAC.[Batch_Lot No_] = '{req_reference_no}'
+                        GROUP BY POAC.[Prod_ Order No_],POC.[Item No_] """
+    # Query Navision for information with referencetype = 4 (lotno) and specified roll number.
+    query_nav_lot_roll = f"""SELECT POAC.[Prod_ Order No_] AS [Produktionsordrenummer]
+                        	,POC.[Item No_] AS [Varenummer], [Roll No_] AS [Rullenummer]	
+                            FROM [dbo].[BKI foods a_s$Prod_ Order Add_ Comp_] AS POAC
+                            INNER JOIN [dbo].[BKI foods a_s$Prod_ Order Component] AS POC
+                            	ON POAC.[Prod_ Order No_] = POC.[Prod_ Order No_]
+                            	AND POAC.[Prod_ Order Line No_] = POC.[Prod_ Order Line No_]
+                            	AND POAC.[Prod_ Order Component Line No_] = POC.[Line No_]
+                            WHERE POAC.[Batch_Lot No_] = '{req_reference_no}'
+                            	AND [Roll No_] = '{req_roll}'
+                            GROUP BY POAC.[Prod_ Order No_],POC.[Item No_] """
+    # Query Navision for information with refencetype = 5 (purchase order) and no roll number. 
+    query_nav_purch = f"""SELECT POAC.[Prod_ Order No_] AS [Produktionsordrenummer]
+                    	,POC.[Item No_] AS [Varenummer], [Roll No_] AS [Rullenummer]
+                        FROM [dbo].[BKI foods a_s$Prod_ Order Add_ Comp_] AS POAC
+                        INNER JOIN [dbo].[BKI foods a_s$Prod_ Order Component] AS POC
+                        	ON POAC.[Prod_ Order No_] = POC.[Prod_ Order No_]
+                        	AND POAC.[Prod_ Order Line No_] = POC.[Prod_ Order Line No_]
+                        	AND POAC.[Prod_ Order Component Line No_] = POC.[Line No_]
+                        WHERE POAC.[Batch_Lot No_] = '{req_reference_no}'
+                        GROUP BY POAC.[Prod_ Order No_],POC.[Item No_] """
+    # Query Navision for information with referencetype = 5 (purchase order) and specified roll number.
+    query_nav_purch_roll = f"""SELECT POAC.[Prod_ Order No_] AS [Produktionsordrenummer]
+                        	,POC.[Item No_] AS [Varenummer], [Roll No_] AS [Rullenummer]	
+                            FROM [dbo].[BKI foods a_s$Prod_ Order Add_ Comp_] AS POAC
+                            INNER JOIN [dbo].[BKI foods a_s$Prod_ Order Component] AS POC
+                            	ON POAC.[Prod_ Order No_] = POC.[Prod_ Order No_]
+                            	AND POAC.[Prod_ Order Line No_] = POC.[Prod_ Order Line No_]
+                            	AND POAC.[Prod_ Order Component Line No_] = POC.[Line No_]
+                            WHERE POAC.[Batch_Lot No_] = '{req_reference_no}'
+                            	AND [Roll No_] = '{req_roll}'
+                            GROUP BY POAC.[Prod_ Order No_],POC.[Item No_] """
+    # Query BKI_Datastore for information with referencetype = 4 (lot no), only for ventil components
+    query_ds_lot_ventil = f"""SELECT [Ordrenummer] AS [Produktionsordrenummer], [Varenummer]
+                              FROM [cof].[Ventil_registrering]
+                              WHERE [Batchnr_stregkode] = '{req_reference_no}'
+                              GROUP BY [Varenummer], [Ordrenummer]"""
+    # Read any of the above queries into a dataframe, depending on reference type, request type and whether any roll number has been entered.
+    if req_type == 4: # Folie
+        if req_reference_type == 4: # Lotnumber
+            if not req_roll: # req_roll holds a value
+                df_generelt = pd.read_sql(query_nav_lot_roll, con_nav)
+            else: # req_roll doesn't hold a value
+                df_generelt = pd.read_sql(query_nav_lot, con_nav)
+        if req_reference_type == 5: # Purchase order
+            if not req_roll: # req_roll holds a value
+                df_generelt = pd.read_sql(query_nav_purch_roll, con_nav)
+            else: # req_roll doesn't hold a value
+                df_generelt = pd.read_sql(query_nav_purch, con_nav)
+    elif req_type == 5: # Karton
+        df_generelt = pd.read_sql(query_nav_lot, con_nav)
+    elif req_type == 6: # Ventil
+        df_generelt = pd.read_sql(query_ds_lot_ventil, con_nav)
+    else:
+        df_generelt = pd.DataFrame()
+        
+    print(df_generelt)
+
+
+    # =============================================================================
+    # Section 1: Generelt
+    # =============================================================================
+    section_id = 1
+    section_name = ssf.get_section_name(section_id, df_sections)
+    column_order = ['Silo', 'Anmodet dato', 'Sidste tommelding'
+                    ,'Efterfølgende tommelding', 'Reworktype']
+
+    df_generelt = df_request
+    if ssf.get_section_status_code(df_generelt) == 99:
+        try:
+            df_generelt['Silo'] = req_reference_no
+            df_generelt['Anmodet dato'] = silo_req_date_dmy
+            df_generelt['Sidste tommelding'] = ssf.convert_date_format(silo_last_empty_ymd, 'yyyy-mm-dd', 'dd-mm-yyyy')
+            df_generelt['Efterfølgende tommelding'] = ssf.convert_date_format(silo_next_empty_ymd, 'yyyy-mm-dd', 'dd-mm-yyyy')
+            df_generelt['Reworktype'] = ssf.rework.get_rework_type(req_reference_no)
+            # Transpose dataframe
+            df_generelt = df_generelt[column_order].transpose()
+            df_generelt = df_generelt.reset_index()
+            df_generelt.columns = ['Sektion','Værdi']
+            # Write results to Excel
+            ssf.insert_dataframe_into_excel(excel_writer, df_generelt, section_name, True)
+            # Write status into log
+            ssf.section_log_insert(req_id, section_id, 0)
+        except Exception as e:
+            # Insert error into log
+            ssf.section_log_insert(req_id, section_id, 2, e)
+    else:
+        # Write into log if no data is found or section is out of scope
+        ssf.section_log_insert(req_id, section_id, ssf.get_section_status_code(df_generelt))

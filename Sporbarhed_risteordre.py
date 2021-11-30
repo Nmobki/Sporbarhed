19c0216c136bda8bffc7aa44a90c42fab2a09bca
Nmobki <nmo@bki.dk>
2021-11-30 08:18:39 +0100

    Update Sporbarhed_risteordre.py

--- Sporbarhed_risteordre.py
+++ Sporbarhed_risteordre.py
@@ -36,14 +36,14 @@ def initiate_report(initiate_id):
     # Set request variables
     # =============================================================================
     req_type = df_request.loc[0, 'Forespørgselstype']
-    req_reference_no = df_request.loc[0, 'Referencenummer'].rstrip(' ')
+    req_order_no = df_request.loc[0, 'Referencenummer']
     req_recipients = df_request.loc[0, 'Rapport_modtager']
     req_note = df_request.loc[0, 'Note_forespørgsel']
     req_id = df_request.loc[0, 'Id']
     req_ordrelationstype = df_request.loc[0, 'Ordrerelationstype']
 
     script_name = 'Sporbarhed_risteordre.py'
-    orders_top_level = [req_reference_no]
+    orders_top_level = [req_order_no]
     orders_related = []
     # Read setup for section for reporttypes. NAV querys with NOLOCK to prevent deadlocks
     df_sections = ssf.get_ds_reporttype(req_type)
@@ -61,7 +61,7 @@ def initiate_report(initiate_id):
     # Variables for files generated
     # =============================================================================
     filepath = ssf.get_filepath('report')
-    file_name = f'Rapport_{req_reference_no}_{req_id}'
+    file_name = f'Rapport_{req_order_no}_{req_id}'
 
     wb_name = f'{file_name}.xlsx'
     path_file_wb = filepath + r'\\' + wb_name
@@ -74,71 +74,58 @@ def initiate_report(initiate_id):
     # Queries for different parts of report
     # =============================================================================
     # Query to read various information from BKI_Datastore for the order requested in the report
-    query_ds_generelt = f""" WITH [KP] AS ( SELECT [Ordrenummer]
-                    	,SUM( CASE WHEN [Prøvetype] = 0 THEN [Antal_prøver] ELSE 0 END) AS [Kontrolprøve]
-                    	,SUM( CASE WHEN [Prøvetype] = 1 THEN [Antal_prøver] ELSE 0 END) AS [Referenceprøve]
-                    	,SUM( CASE WHEN [Prøvetype] = 2 THEN [Antal_prøver] ELSE 0 END) AS [Henstandsprøve]
-                        FROM [cof].[Kontrolskema_prøver]
-                        GROUP BY [Ordrenummer] )
-                        ,[SK] AS ( SELECT [Referencenummer] ,MAX([Status]) AS [Status]
-                        FROM [cof].[Smageskema] WHERE [Referencetype] = 2
-                        GROUP BY [Referencenummer] )
-                        SELECT SF.[Referencenummer] AS [Ordrenummer] ,SF.[Pakketidspunkt] ,KH.[Igangsat_af] AS [Igangsat af]
-                        ,KH.[Silo_opstart] AS [Opstartssilo] ,KH.[Taravægt] ,KH.[Nitrogen] / 100.0 AS [Nitrogen]
-                        ,KH.[Bemærkning] AS [Bemærkning opstart] ,ISNULL(KP.[Kontrolprøve] ,0) AS [Kontrolprøver]
-                        ,ISNULL(KP.[Referenceprøve] ,0) AS [Referenceprøver]
-                        ,ISNULL(KP.[Henstandsprøve] ,0) AS [Henstandsprøver]
-                        ,CASE WHEN SK.[Status] = 1 THEN 'Godkendt' WHEN SK.[Status] = 0 THEN 'Afvist'
-                        ELSE 'Ej smagt' END AS [Smagning status], KH.[Pakkelinje]
-                        FROM [trc].[Sporbarhed_forespørgsel] AS SF
-                        LEFT JOIN [cof].[Kontrolskema_hoved] AS KH 
-                            ON SF.[Referencenummer] = KH.[Ordrenummer]
-                        LEFT JOIN [KP] 
-                            ON SF.[Referencenummer] = KP.[Ordrenummer]
-                        LEFT JOIN [SK] 
-                            ON SF.[Referencenummer] = SK.[Referencenummer]
-                        WHERE SF.[Id] = {req_id} """
-    df_results_generelt = pd.read_sql(query_ds_generelt, con_ds)
+    query_generelt = f""" SELECT [S_CUSTOMER_CODE] AS [Receptnummer]
+                    	 ,DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0) AS [Dato]
+                    	 ,[PRODUCTION_ORDER_ID] AS [Probat id] ,[SOURCE_NAME] AS [Rister]
+                    	 ,[DEST_NAME] AS [Silo] ,SUM([WEIGHT] / 1000.0) AS [Kilo]
+                         FROM [dbo].[PRO_EXP_ORDER_UNLOAD_R]
+                         WHERE [ORDER_NAME] = '{req_order_no}'
+                         GROUP BY [S_CUSTOMER_CODE] ,DATEADD(D, DATEDIFF(D, 0, [RECORDING_DATE] ), 0)
+                    	,[PRODUCTION_ORDER_ID] ,[SOURCE_NAME],[DEST_NAME] """
+    df_generelt = pd.read_sql(query_generelt, con_probat)
 
     # =============================================================================
     # Section 1: Generelt
     # =============================================================================
     section_id = 1
     section_name = ssf.get_section_name(section_id, df_sections)
-    column_order = []
-    columns_1_dec = []
+    column_order = ['Receptnummer', 'Receptnavn', 'Farve sætpunkt', 'Vandprocent sætpunkt',
+                    'Dato', 'Rister', 'Probat id', 'Silo', 'Kilo']
+    columns_1_dec = ['Kilo']
+    columns_strip = ['Dato','Silo']
 
-    if ssf.get_section_status_code(df_nav_generelt) == 99:
+    if ssf.get_section_status_code(df_generelt) == 99:
         try:
-            df_nav_generelt['Pakkelinje'] = df_results_generelt['Pakkelinje'].iloc[0]
-            df_nav_generelt['Pakketidspunkt'] = df_results_generelt['Pakketidspunkt'].iloc[0]
-            df_nav_generelt['Ordrenummer'] = req_reference_no
-            df_nav_generelt['Smagning status'] = df_results_generelt['Smagning status'].iloc[0]
-            df_nav_generelt['Opstartssilo'] = df_results_generelt['Opstartssilo'].iloc[0]
-            df_nav_generelt['Igangsat af'] = df_results_generelt['Igangsat af'].iloc[0]
-            df_nav_generelt['Taravægt'] = df_results_generelt['Taravægt'].iloc[0]
-            df_nav_generelt['Nitrogen'] = df_results_generelt['Nitrogen'].iloc[0]
-            df_nav_generelt['Lotnumre produceret'] = len(df_nav_lotno)
-            df_nav_generelt['Henstandsprøver'] = df_results_generelt['Henstandsprøver'].iloc[0]
-            df_nav_generelt['Referenceprøver'] = df_results_generelt['Referenceprøver'].iloc[0]
-            df_nav_generelt['Kontrolprøver'] = df_results_generelt['Kontrolprøver'].iloc[0]
-            df_nav_generelt['Bemærkning opstart'] = df_results_generelt['Bemærkning opstart'].iloc[0]
+            df_generelt = ''
             # Apply column formating
             for col in columns_1_dec:
-                df_nav_generelt[col] = df_nav_generelt[col].apply(lambda x: ssf.number_format(x, 'dec_1'))
-            df_nav_generelt['Produktionsdato'] = df_nav_generelt['Produktionsdato'].dt.strftime('%d-%m-%Y')
+                df_generelt[col] = df_generelt[col].apply(lambda x: ssf.number_format(x, 'dec_1'))
+            df_generelt['Produktionsdato'] = df_generelt['Produktionsdato'].dt.strftime('%d-%m-%Y')
+            # Group columns TODO!!!!!!!!!!!!!!!!!!!!!!!!
+            df_generelt = df_generelt.groupby(['Receptnummer','Rister', 'Probat id']).agg(
+               {'Dato': lambda x: ','.join(sorted(pd.Series.unique(x))),
+                'Silo': lambda x: ','.join(sorted(pd.Series.unique(x))),
+                'Kilo': 'sum'
+               }).reset_index()
+            # Remove trailing and leading commas
+            for col in columns_strip:
+                df_generelt[col] = df_generelt[col].apply(lambda x: ssf.strip_comma_from_string(x))
+            # Lookup values from item table
+            df_generelt['Receptnavn'] = df_generelt.apply(lambda x: ssf.get_nav_item_info(x.Receptnummer, 'Beskrivelse'), axis=1)
+            df_generelt['Farve sætpunkt'] = df_generelt.apply(lambda x: ssf.get_nav_item_info(x.Receptnummer, 'Farve'), axis=1)
+            df_generelt['Vandprocent sætpunkt'] = df_generelt.apply(lambda x: ssf.get_nav_item_info(x.Receptnummer, 'Vandprocent'), axis=1)
             # Transpose dataframe
-            df_nav_generelt = df_nav_generelt[column_order].transpose()
-            df_nav_generelt = df_nav_generelt.reset_index()
-            df_nav_generelt.columns = ['Sektion','Værdi']
+            df_generelt = df_generelt[column_order].transpose()
+            df_generelt = df_generelt.reset_index()
+            df_generelt.columns = ['Sektion','Værdi']
             # Write results to Excel
-            ssf.insert_dataframe_into_excel(excel_writer, df_nav_generelt, section_name, True)
+            ssf.insert_dataframe_into_excel(excel_writer, df_generelt, section_name, True)
             # Write status into log
             ssf.section_log_insert(req_id, section_id, 0)
         except Exception as e: # Insert error into log
             ssf.section_log_insert(req_id, section_id, 2, e)
     else: # Write into log if no data is found or section is out of scope
-        ssf.section_log_insert(req_id, section_id, ssf.get_section_status_code(df_nav_generelt))
+        ssf.section_log_insert(req_id, section_id, ssf.get_section_status_code(df_generelt))
 
 
 
@@ -172,7 +159,7 @@ def initiate_report(initiate_id):
     dict_email_log = {'Filsti': filepath
                       ,'Filnavn': file_name
                       ,'Modtager': req_recipients
-                      ,'Emne': ssf.get_email_subject(req_reference_no, req_type)
+                      ,'Emne': ssf.get_email_subject(req_order_no, req_type)
                       ,'Forespørgsels_id': req_id
                       ,'Note':req_note}
     pd.DataFrame(data=dict_email_log, index=[0]).to_sql('Sporbarhed_email_log', con=engine_ds, schema='trc', if_exists='append', index=False)
